79a7bcc82cea4693f69638009943c60bd81017e1
luispneu <luisgabriel8328@gmail.com>
2023-06-14 17:35:40 -0400

    falta terminar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+class MeuError extends Error{
+  constructor(menssage){
+  super(menssage);
+  this.name = "Meu erro";
+  }
+}
+
 class Produto {
     constructor(nome, dataCadastro, descricao, preco) {
       this.nome = nome;
@@ -11,7 +18,7 @@ class Produto {
     }
   }
   
-  const produto = new Produto("corta vento palmeiras", "2023", "cor verde e branco", "346");
+  const produto = new Produto("<h1>Calça moletom palmeiras<br>", "2023<br>", "cor verde e branco<br>", "346R$");
   console.log(produto.mostrarProduto());
   
   class ProdutoDestaque extends Produto {
@@ -19,23 +26,41 @@ class Produto {
       super(nome, dataCadastro, descricao, preco);
       this.imagemDestaque = imagemDestaque;
     }
+
+    mostrarAtributos(){
+      try{
+        return this.atri1butos();
+      } catch(erro){
+        console.log(erro.stack)
+      }
+    } 
   
     mostrarProdutoDestaque() {
       return `
-        <div>${this.nome}</div><h4><br>
+        <h1><div>${this.nome}</div><br>
         <img src="${this.imagemDestaque}">
         <div>${this.dataCadastro}</div>
         <div>${this.descricao}</div>
-        <div>${this.preco}</div><br>`;
+        <div>${this.preco}</div>`;
+    }
+
+    atributos() {
+      if (this.nome != "" && this.imagemDestaque != "" && this.dataCadastro!= ""  && this.descricao!= ""  && this.preco!= "")
+      {return {
+        nome:this.nome,
+        imagemDestaque:this.imagemDestaque,
+      dataCadastro:this.dataCadastro,
+      descricao:this.descricao,
+      preco:this.preco
+      }; } else{ 
+      throw  new MeuError("todos os atributos tem que estar completos")
+    }
     }
   }
   
-  const produto2 = new Produto("Capinha Celular do palmeras<h2>",  "Capinha de Xiaomi", "100R$");
-  const produto3 = new Produto("Moletom com Simbolo do palmeiras<br>", "2023-06-10", "Moletom do palmeiras", "200R$");
-  
-  const produtoDestaque = new ProdutoDestaque("Corta vento palmeiras", "2023-06-10", "Lindo corta vento", "346R$", "https://d3ugyf2ht6aenh.cloudfront.net/stores/002/044/094/products/022763541-38bd6da7d097e9e2eb16468838991966-640-0.jpg");
+  const produto3 = new Produto("", "2023-06-10", "200R$");
+  const produtoDestaque = new ProdutoDestaque("Corta vento palmeiras", "2023-06-10", "Verde e branco", "346R$", "https://d3ugyf2ht6aenh.cloudfront.net/stores/002/044/094/products/022763541-38bd6da7d097e9e2eb16468838991966-640-0.jpg");
   console.log(produto.mostrarProduto());
-  console.log(produto2.mostrarProduto());
   console.log(produtoDestaque.mostrarProdutoDestaque());
   
   const produto1 = document.getElementById("lista-produtos");
